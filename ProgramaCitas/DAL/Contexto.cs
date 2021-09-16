e71b1bc5a8f3afba3b9b159552deec6ab8d8cfe7
Crismeyri Gutierrez Gil <crismeyri_gutierrez@ucne.edu.do>
2021-09-15 22:45:58 -0400

    Agregando las entidades

---
+++ ProgramaCitas/DAL/Contexto.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProgramaCitas.Entidades;
+
+namespace ProgramaCitas.DAL
+{
+    public class Contexto: DbContext
+    {
+        public DbSet<Citas> cita { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = Data\citas.db");
+        }
+    }
+}
---
+++ ProgramaCitas/Entidades/Cita.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace ProgramaCitas.Entidades
+{
+    public class Cita
+    {
+        [Key]
+        public int UsuarioId { get; set; }
+        public string CitaId { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Telefono { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Descripcion { get; set; }
+
+    }
+}
