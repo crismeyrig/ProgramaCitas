4db6beab5e74840acee6ecf2702a414e30b516f4
Crismeyri Gutierrez Gil <crismeyri_gutierrez@ucne.edu.do>
2021-09-18 22:30:54 -0400

    Corrigiendo

--- ProgramaCitas/DAL/Contexto.cs
+++ ProgramaCitas/DAL/Contexto.cs
@@ -1,12 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using ProgramaCitas.Entidades;
-using ProgramaCitas.UI.Registros;
-
+using System;
 
 namespace ProgramaCitas.DAL
 {
